c433f922e95c8ace42b2636d06581473e0589985
kajeta <ezikeohaking@gmail.com>
2022-04-11 14:02:25 +0100

    firstprop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Profile from "./Profile/Profile";
 
 function App() {
+  function handleName() {
+    alert(this.fullName);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "flex", justifyContent: "center" }}>
+      <Profile
+        fullName="kajeta"
+        handle={handleName}
+        bio="i love watching movies, coding and musiccc plus twitter lol "
+        profession="Software Developer"
+      >
+        <img style={{ width: "8rem" }} alt="timer" src={logo} />
+      </Profile>
     </div>
   );
 }
